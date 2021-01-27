f2883c94229090a6b39a21a7cf6639abfa2a48d4
Abdellatif CHALAL <abdellatifchalal44@gmail.com>
2021-01-27 22:53:55 +0100

    done UnitTest and Intergration test

---
+++ .history/app/Http/Services/ToDoListService_20210127215408.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Mail\emailToUser;
+use App\Models\Item;
+use App\Models\TodoList;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Mail;
+
+class ToDoListService
+{
+
+    public function createToDoList(User $user, string $description): TodoList
+    {
+        if (is_null($user)) {
+            throw new Exception("User connot be null");
+        }
+        try {
+            $user->isValid();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        $user->todolist()->create([
+            "description" => $description
+        ]);
+        return $user->todolist;
+    }
+
+    public function add(User $user, Item $item): Item
+    {
+        if (is_null($user)) {
+            throw new Exception("User connot be null");
+        }
+        try {
+            $user->isValid();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        if (is_null($item)) {
+            throw new Exception("Item connot be null");
+        }
+        try {
+            $item->isValid();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        if ($user->todolist->getNumberOfItemsOfUser() === 0) {
+            $item = $this->addToDatabase($user, $item);
+            return $item;
+        }
+
+        $this->checkUniqueItemName($user, $item);
+
+
+        // if (!$user->todolist->checkPeriodBetweenTwoItems($item)) {
+        //     throw new Exception("more 30 minutes if you want add a new item !!");
+        // }
+
+        // if ($user->todolist->getNumberOfItemsOfUser() >= 10) {
+        //     throw new Exception("You have too many items !!");
+        // }
+
+        // if ($user->todolist->getNumberOfItemsOfUser() + 1 === 8) {
+        //     $this->sendEmail($user);
+        // }
+
+
+        // $item = $this->addToDatabase($user, $item);
+        return $item;
+    }
+
+    public function sendEmail($user): bool
+    {
+        return Mail::to($user->email)->send(new emailToUser());
+    }
+
+    public function addToDatabase($user, $item): Item
+    {
+        $item = $user->todolist->items()->create([
+            "name" => $item->name,
+            "content" => $item->content,
+        ]);
+        return $item;
+    }
+
+    public function checkUniqueItemName($user, $item): bool
+    {
+        foreach ($user->todolist->items as $todolistItem) {
+            if ($todolistItem->name === $item->name) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ .history/tests/Unit/ToDoListTest_20210127221003.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Services\ToDoListService;
+use App\Models\Item;
+use App\Models\TodoList;
+use App\Models\User;
+use Carbon\Carbon;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertTrue;
+
+class ToDoListTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    private Item $item;
+    private User $user;
+    private $todoListservice;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = new User([
+            'first_name' => "abdellatif",
+            'last_name' => "chalal",
+            "date_naissance" => Carbon::now()->subYears(13)->toDateString(),
+            'email' => "test@gmail.com",
+            'password' => 'password',
+
+        ]);
+        $this->item = new Item([
+            "name" => "name",
+            "content" => "content",
+        ]);
+
+        $this->user->todolist = $this->getMockBuilder(TodoList::class)
+            ->onlyMethods(['getNumberOfItemsOfUser', 'checkPeriodBetweenTwoItems'])
+            ->getMock();
+
+
+        $this->todoListservice = $this->getMockBuilder(ToDoListService::class)
+            ->onlyMethods(['addToDatabase', 'sendEmail', 'checkUniqueItemName'])
+            ->getMock();
+        $this->todoListservice->expects($this->any())->method('sendEmail')->willReturn(true);
+        $this->todoListservice->expects($this->any())->method('addToDatabase')->willReturn($this->item);
+        $this->todoListservice->expects($this->any())->method('checkUniqueItemName')->willReturn(true);
+    }
+
+
+    public function test_add_normal_Item_()
+    {
+        $this->user->todolist->expects($this->any())->method('getNumberOfItemsOfUser')->willReturn(1);
+        $this->user->todolist->expects($this->any())->method('checkPeriodBetweenTwoItems')->willReturn(true);
+        assertEquals($this->item, $this->todoListservice->add($this->user, $this->item));
+    }
+
+    public function test_add_item_after_30_minutes()
+    {
+        $this->user->todolist->expects($this->any())->method('getNumberOfItemsOfUser')->willReturn(1);
+        $this->user->todolist->expects($this->any())->method('checkPeriodBetweenTwoItems')->willReturn(false);
+
+        $this->expectException('Exception');
+        $this->expectExceptionMessage('more 30 minutes if you want add a new item !!');
+        assertEquals($this->item, $this->todoListservice->add($this->user, $this->item));
+    }
+
+    public function test_add_item_at_8th_item_to_send_email_to_user()
+    {
+        $this->user->todolist->expects($this->any())->method('getNumberOfItemsOfUser')->willReturn(7);
+        $this->user->todolist->expects($this->any())->method('checkPeriodBetweenTwoItems')->willReturn(true);
+
+        assertEquals($this->item, $this->todoListservice->add($this->user, $this->item));
+    }
+
+
+    //     public function testAddItemMore10Items()
+    //     {
+
+    //         $this->todolist->expects($this->any())->method('getNumberOfItemsOfUser')->willReturn(10);
+    //         $this->expectException('Exception');
+    //         $this->expectExceptionMessage('You have too many items !!');
+
+    //         assertTrue($this->todolist->add($this->item));
+    //     }
+}
